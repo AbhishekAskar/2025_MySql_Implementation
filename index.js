673c3a46c9ac6f380f763500ca52e6de938fa3aa
Abhishek <abhiaskar.18@gmail.com>
2025-05-15 15:14:30 +0530

    Bus Booking System initial database

--- index.js
+++ index.js
@@ -10,35 +10,54 @@ const connection = mysql.createConnection({
     database: 'testdb'
 })
 
-connection.connect((err) =>{
-    if(err){
+connection.connect((err) => {
+    if (err) {
         console.log(err);
         return;
     }
 
     console.log("Connection has been created");
 
-    const creationQuery = `create table Students(
+    const tables = [
+    `CREATE TABLE IF NOT EXISTS Users (
         id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(20),
         email VARCHAR(20)
+    )`,
+    `CREATE TABLE IF NOT EXISTS Buses (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        busNumber INT,
+        totalSeats INT,
+        availableSeats INT
+    )`,
+    `CREATE TABLE IF NOT EXISTS Bookings (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        seatNumbers INT
+    )`,
+    `CREATE TABLE IF NOT EXISTS Payments (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        amountPaid INT,
+        paymentStatus VARCHAR(20)
     )`
+];
 
-    connection.execute(creationQuery, (err)=>{
-        if(err){
-            console.log(err);
-            connection.end();
-            return;
+
+    tables.forEach((query) => {
+    connection.query(query, (err, result) => {
+        if (err) {
+            console.error("Error executing query:", err.message);
+        } else {
+            console.log("Table created successfully.");
         }
-
-        console.log("Table is created");
     });
+});
+
 })
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Hello Abhishek")
 })
 
-app.listen(port, (req, res)=>{
+app.listen(port, (req, res) => {
     console.log("The is listening on Port: " + port);
 })
